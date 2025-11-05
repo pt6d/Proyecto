d4fd17815981eb5ecea51433f6455177d74534fd
erick-g06 <erickgoes0206@gmail.com>
2025-11-05 07:38:44 -0600

    Cree las cartas

--- Program.cs
+++ Program.cs
@@ -1,9 +1,64 @@
-﻿namespace Proyecto;
+﻿using System;
+//using Proyecto.Clases;
+//using Proyecto.Interfaces;
 
-class Program
+namespace Proyecto
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main()
+        {
+            bool salir = false;
+
+            while (!salir)
+            {
+                Console.Clear();
+                Console.WriteLine("-------- Menu --------");
+                Console.WriteLine("1. BlackJack");
+                Console.WriteLine("2. UNO");
+                Console.WriteLine("3. Salir");
+                Console.WriteLine("Seleccione una opcion:");
+
+                try
+                {
+                    int opcion = int.Parse(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Eduardo es gay");
+                            //IniciarBlackJack();
+                            Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Gabriel es gay");
+                            //IniciarUNO();
+                            Console.ReadLine();
+                            break;
+                        case 3:
+                            salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("ERROR, opción invalida");
+                            Console.ReadLine();
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Opcion no valida");
+                    Console.ReadLine();
+                }
+            }
+
+
+
+        }
+
+
+
+
+
     }
+
 }
